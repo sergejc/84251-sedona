ec3f2ea9d6cdc57cea2cc4bb9b75a065546f179a
Sergej Charskij <sergej.charskij@gmail.com>
2018-05-10 22:56:33 +0100

    Added the google maps

--- source/js/app.js
+++ source/js/app.js
@@ -7,3 +7,16 @@ toggleButton.addEventListener('click', function(e){
   this.classList.toggle('main-nav__toggle-open');
   menu.classList.toggle('main-nav__list--open');
 });
+
+function initMap() {
+  var uluru = {lat: 34.7858293, lng: -111.8954255};
+  var map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 9,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: "img/icon-map-marker.svg"
+  });
+}
