abc1b944cf7c97a01cf37a770cd60a91fc184902
Sergej Charskij <sergej.charskij@gmail.com>
2018-05-15 20:28:06 +0100

    Update pages to make them compatible with B5

--- source/js/app.js
+++ source/js/app.js
@@ -4,7 +4,7 @@ var menu = document.querySelector('.main-nav__list');
 document.body.classList.remove('no-js');
 
 toggleButton.addEventListener('click', function(e){
-  this.classList.toggle('main-nav__toggle-open');
+  this.classList.toggle('main-nav__toggle--open');
   menu.classList.toggle('main-nav__list--open');
 });
 
