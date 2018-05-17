5cf6e1273b8cad609255058469ceb7cce4c53b36
Sergej Charskij <sergej.charskij@gmail.com>
2018-05-17 23:15:33 +0100

    Added the gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,47 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var htmlmin = require('gulp-html-minifier');
 
 gulp.task("style", function() {
-  gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    gulp.src("source/sass/style.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest("build/css"))
+        .pipe(server.stream());
+});
+
+gulp.task('minifyhtml', function() {
+    gulp.src('./source/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./build'))
+});
+
+gulp.task("copy", function(){
+    gulp.src([
+        './source/img/**',
+        './source/fonts/**',
+        './source/js/**',
+    ],{
+        base: 'source'
+    })
+        .pipe(gulp.dest('./build/'));
 });
 
 gulp.task("serve", ["style"], function() {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+    server.init({
+        server: "source/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+    gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+    gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("build", ["style", "minifyhtml", "copy"]);
